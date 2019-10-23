38e44a544d2341905eb32eaa50a5b9805db2a76a
aima-z <aimkan86@gmail.com>
2019-10-23 10:58:00 -0500

    day 4

---
+++ src/test/java/tests/day4/FindElementsTest.java
@@ -0,0 +1,27 @@
+package tests.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserFactory;
+import utils.BrowserUtils;
+
+public class FindElementsTest {
+
+    public static void main(String[] args) {
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        //step1. open inspector on chrome and find locator for the element
+        //step2. create object of webelement
+        //step3. use webelement
+
+        WebElement button = driver.findElement(By.id("form_submit"));
+        //to click on that element
+        button.click();
+        BrowserUtils.wait(2);
+        driver.close();
+
+
+
+    }
+}
